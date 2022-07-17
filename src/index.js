b6b1374f92102f3bf88fda219fd748e731d3dc01
Anastasiya Shvedun <nastya.shvedun.03@mail.ru>
2022-07-17 20:10:36 +0300

    done reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = Math.abs(n)
+    let arr = num.toString(10).split('').map(int => parseInt(int, 10))
+    let ReverseArray = arr.reverse()
+    return Number(ReverseArray.join(''))
 }
